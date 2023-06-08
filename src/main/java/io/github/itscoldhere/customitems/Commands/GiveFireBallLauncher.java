07cc8c822a7afeee52e509a594fbad6a506dc613
itscoldhere <atlior3@gmail.com>
2023-06-08 16:18:18 +0300

    idk

---
+++ src/main/java/io/github/itscoldhere/customitems/Commands/GiveFireBallLauncher.java
@@ -0,0 +1,20 @@
+package io.github.itscoldhere.customitems.Commands;
+
+import io.github.itscoldhere.customitems.Items.FireBallLauncher;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class GiveFireBallLauncher implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if (commandSender instanceof Player){
+            Player player = (Player) commandSender;
+            FireBallLauncher fr = new FireBallLauncher();
+            player.getInventory().addItem(fr.getItemStack());
+        }
+        return false;
+    }
+}
---
+++ src/main/java/io/github/itscoldhere/customitems/Items/FireBallLauncher.java
@@ -0,0 +1,35 @@
+package io.github.itscoldhere.customitems.Items;
+
+import io.github.itscoldhere.customitems.Util.ItemUtil;
+import io.github.itscoldhere.customitems.Util.RegisteryUtil;
+import org.bukkit.Material;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class FireBallLauncher implements ItemUtil {
+    @Override
+    public void onRightClick(PlayerInteractEvent var1) {
+        Player player = var1.getPlayer();
+        player.launchProjectile(Fireball.class, player.getLocation().getDirection());
+    }
+
+    @Override
+    public void onLeftClick(PlayerInteractEvent var1) {}
+
+    @Override
+    public @NotNull ItemStack getItemStack() {
+        RegisteryUtil Util = new RegisteryUtil(Material.STICK, "FireBall Launcher", "fireballlauncher", "A powerful tool that let's you become a being of fire", "With this tool you can shoot fireballs", "Right click to shoot fireballs");
+        Util.setCustomModelData(44);
+        return Util.GetItemStack();
+    }
+
+    @Override
+    public @NotNull List<String> getLore() {
+        return this.getItemStack().getItemMeta().getLore();
+    }
+}
